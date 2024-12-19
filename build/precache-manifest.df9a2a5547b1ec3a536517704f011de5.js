68b3665c2db72e8ed52acabd57e7c970e0ec1de3
Yuval Berghaus <yuval@comunityapp.com>
2024-12-19 17:00:31 -0500

    build is ready

---
+++ build/precache-manifest.df9a2a5547b1ec3a536517704f011de5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ab6a994c7901c8a92e85906789b062",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc45effa695bf00f0fcc",
+    "url": "/static/css/main.1c2f3f75.chunk.css"
+  },
+  {
+    "revision": "822604e967c44ea95238",
+    "url": "/static/js/2.86a293ab.chunk.js"
+  },
+  {
+    "revision": "5ad5544b95d92d2643cd3d0c6a870b6d",
+    "url": "/static/js/2.86a293ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc45effa695bf00f0fcc",
+    "url": "/static/js/main.af9f9491.chunk.js"
+  },
+  {
+    "revision": "e05473f59f7762fe465a",
+    "url": "/static/js/runtime-main.07b1ca12.js"
+  },
+  {
+    "revision": "13e8aa0ecf49b915930f355884e8e73c",
+    "url": "/static/media/landing-logo.13e8aa0e.png"
+  },
+  {
+    "revision": "1607703c5f2033209a2bb025ed9a3f4d",
+    "url": "/static/media/mockup.1607703c.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a87dfabfaceca2db6589d2b92967ab4.js"
+  "/precache-manifest.df9a2a5547b1ec3a536517704f011de5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
