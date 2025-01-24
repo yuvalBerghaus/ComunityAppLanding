9abc2666fcb265428c1631bcf2c01dacafc778f1
Yuval Berghaus <yuval@comunityapp.com>
2025-01-24 15:31:11 -0500

    fixed nvm

---
+++ build/precache-manifest.15a2d3f60907468f805c1f66eaacc8c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b44242128d1ed511bcd6aad82c40d53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a4db6aff81beb730163",
+    "url": "/static/css/main.1c2f3f75.chunk.css"
+  },
+  {
+    "revision": "64310cb56e11e8939be1",
+    "url": "/static/js/2.0de71d34.chunk.js"
+  },
+  {
+    "revision": "5ad5544b95d92d2643cd3d0c6a870b6d",
+    "url": "/static/js/2.0de71d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4db6aff81beb730163",
+    "url": "/static/js/main.28111fdb.chunk.js"
+  },
+  {
+    "revision": "e05473f59f7762fe465a",
+    "url": "/static/js/runtime-main.07b1ca12.js"
+  },
+  {
+    "revision": "13e8aa0ecf49b915930f355884e8e73c",
+    "url": "/static/media/landing-logo.13e8aa0e.png"
+  },
+  {
+    "revision": "1607703c5f2033209a2bb025ed9a3f4d",
+    "url": "/static/media/mockup.1607703c.png"
+  }
+]);
